c11742f704071d7fbbd418d7f463491966107562
Julius Pedersen <julius.pedersen@origo.oslo.kommune.no>
2024-01-04 18:54:06 +0100

    ✅ Add webcam handling

--- cmd/start-webcam/api.go
+++ cmd/start-webcam/api.go
@@ -1,6 +1,12 @@
 package startwebcam
 
-import "github.com/spf13/cobra"
+import (
+	"context"
+	"fmt"
+
+	"github.com/deifyed/gpctl/pkg/gopro"
+	"github.com/spf13/cobra"
+)
 
 type Options struct {
 	TargetDeviceIndex int
@@ -8,6 +14,25 @@ type Options struct {
 
 func RunE(opts *Options) func(cmd *cobra.Command, args []string) error {
 	return func(cmd *cobra.Command, args []string) error {
+		deviceAddress, err := getDeviceAddressByIndex(cmd.Context(), opts.TargetDeviceIndex)
+		if err != nil {
+			return fmt.Errorf("getting device address: %w", err)
+		}
+
+		err = gopro.StartWebcam(deviceAddress)
+		if err != nil {
+			return fmt.Errorf("starting webcam: %w", err)
+		}
+
 		return nil
 	}
+}
+
+func getDeviceAddressByIndex(ctx context.Context, index int) (string, error) {
+	devices, err := gopro.GetDeviceAddresses(ctx)
+	if err != nil {
+		return "", fmt.Errorf("getting device addresses: %w", err)
+	}
+
+	return devices[index], nil
 }
---
+++ pkg/gopro/webcam.go
@@ -0,0 +1,32 @@
+package gopro
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func StartWebcam(hostAddress string) error {
+	resp, err := http.Get(fmt.Sprintf("http://%s/gopro/webcam/start", hostAddress))
+	if err != nil {
+		return fmt.Errorf("calling API: %w", err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("API returned non-200 status code: %d", resp.StatusCode)
+	}
+
+	return nil
+}
+
+func StopWebcam(hostAddress string) error {
+	resp, err := http.Get(fmt.Sprintf("http://%s/gopro/webcam/stop", hostAddress))
+	if err != nil {
+		return fmt.Errorf("calling API: %w", err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("API returned non-200 status code: %d", resp.StatusCode)
+	}
+
+	return nil
+}
