c0f28dc75b56be2fa3283bc97497831fe56d474b
Julius Pedersen <julius@tuuturu.io>
2024-07-20 11:28:59 +0200

    🐛 Fix another incorrect var reference

--- cmd/rm.go
+++ cmd/rm.go
@@ -20,5 +20,5 @@ func init() {
 	rootCmd.AddCommand(rmCmd)
 
 	rmCmd.Flags().IntVarP(&rmOpts.TargetDeviceIndex, "device", "d", 0, "Index of the device to use (default is 0)")
-	cpCmd.Flags().BoolVarP(&rmOpts.Progressbar, "progressbar", "p", true, "Show progressbar (default is true)")
+	rmCmd.Flags().BoolVarP(&rmOpts.Progressbar, "progressbar", "p", true, "Show progressbar (default is true)")
 }
